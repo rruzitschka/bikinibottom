d827c702f0fc69719e4421320fe86cb769eab28b
RobertR <robert.ruzitschka@bydeluxe.com>
2018-05-09 22:02:40 +0200

    removed synopsis length parameter, whole synopsis JSON is returned. CLient cn select.

--- RRNodeAPI/routes/assetsynopsis.js
+++ RRNodeAPI/routes/assetsynopsis.js
@@ -5,10 +5,12 @@
 var request = require ('request');
 var elasticSearch = require('elasticsearch');
 
-module.exports = function(assetsID, synopsisLanguage, synopsisLength) {
+module.exports = function(assetsID, synopsisLanguage) {
  
   return new Promise(function (resolve, reject){
-    
+   
+    //create ElasticSearch Client
+
     var client = new elasticSearch.Client ({
       host: 'https://search-sve-test-bdwhx3oa447ij3yezaokfdeup4.us-east-1.es.amazonaws.com/mediaasset',
       log: 'info'
@@ -19,6 +21,9 @@ module.exports = function(assetsID, synopsisLanguage, synopsisLength) {
 
   
     console.log('assetID in assetsynopsis.js: ' + assetsID);
+
+//create asynchronuous search request that looks for the asset with the proper assetID
+
     client.search({
       body:{
         query: {
@@ -29,9 +34,35 @@ module.exports = function(assetsID, synopsisLanguage, synopsisLength) {
       }
       
     }).then(function (body) {
-      hits = body.hits.hits;
-      resultSynopsis = hits[0]._source.mediaLangs[0].synopsis[synopsisLength];
+     // store all hits found in the body, in this case it should be just one
+     
+        hits = body.hits.hits;
+     
+     // store the mediaLang JSON array    
+
+      let mediaLangs = hits[0]._source.mediaLangs;
+      let i = 0;
+      let langIndex;
+      let found = false;
+    // iterate through mediaLang array to find the proper index number for the requested synopsis language
+    // if match is found, store index value
+      for(i; i < mediaLangs.length; i++){
+          if(mediaLangs[i].langID === synopsisLanguage){
+              langIndex = i;
+              found = true;
+          }
+
+      }
+    // if no match is found, use language with index 0 as default
+      if (found !== true){
+          langIndex = 0;
+      }
+
+    // fetch proper synopsis attribute from object
+
+      resultSynopsis = hits[0]._source.mediaLangs[langIndex].synopsis;
       console.log(resultSynopsis);
+    // resolve promise with synopsis array  
       resolve(resultSynopsis);
     }, function (error) {
       console.trace(error.message);
