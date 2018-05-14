36d41735f75e8e05280dff6d2f2ec63c5a78ca7c
RobertR <robert.ruzitschka@bydeluxe.com>
2018-05-14 11:55:04 +0200

    Refactored to call AssetID to get asset data

--- RRNodeAPI/routes/assetcrew.js
+++ RRNodeAPI/routes/assetcrew.js
@@ -1,59 +1,35 @@
-// This function retrieves the crew for the asset witht the specified ID
+// This function retrieves the crew for the asset with the specified ID
 // query parameters: lang  (language) 
 "use strict";
 
 var request = require ('request');
 var elasticSearch = require('elasticsearch');
 var mediaLangIndex = require('../findlangindex.js');
+var getAssetbyID = require('./assetid.js');
 
-module.exports = function(assetsID, synopsisLanguage) {
+module.exports = function(assetID, language) {
  
   return new Promise(function (resolve, reject){
    
-    //create ElasticSearch Client
+    getAssetbyID(assetID, language)
+    .then(function (hits) {
 
-    var client = new elasticSearch.Client ({
-      host: 'https://search-sve-test-bdwhx3oa447ij3yezaokfdeup4.us-east-1.es.amazonaws.com/mediaasset',
-      log: 'info'
-    });
-  
-    var hits;
-    var resultCrew;
-
-  
-    console.log('assetID in assetcrew.js: ' + assetsID);
-
-//create asynchronuous search request that looks for the asset with the proper assetID
-
-    client.search({
-      body:{
-        query: {
-          match: {
-            _id  : assetsID
-          }
-        }
-      }
-      
-    }).then(function (body) {
-     // store all hits found in the body, in this case it should be just one
-     
-        hits = body.hits.hits;
-     
      // store the mediaLang JSON array    
-
+      let resultCrew;
       let mediaLangs = hits[0]._source.mediaLangs;
 
     // find the mediaLang array index number of the requested language
-      let langIndex = mediaLangIndex(mediaLangs,synopsisLanguage);
+      let langIndex = mediaLangIndex(mediaLangs,language);
 
-    // fetch proper synopsis attribute from object
+    // fetch proper crew array from object
 
       resultCrew = hits[0]._source.mediaLangs[langIndex].crew;
       console.log(resultCrew);
-    // resolve promise with synopsis array  
+    // resolve promise with crew array  
       resolve(resultCrew);
-    }, function (error) {
-      console.trace(error.message);
+    })
+    .catch (function (error) {
+      reject(error);
     });
     
   })
