c2269ed5fa52444d73a68668869dde81131ccb2b
RobertR <robert.ruzitschka@bydeluxe.com>
2018-05-18 22:45:21 +0200

    added response handling based on alexa format parameter

--- RRNodeAPI/routes/assets.js
+++ RRNodeAPI/routes/assets.js
@@ -1,12 +1,15 @@
 
+// fetches the list of matching assets based on the query string from ElasticSearch
+
 var request = require ('request');
 var elasticSearch = require('elasticsearch');
 var ESIndexURL = require('./indexURL.js').ES_URL;
+var formatAlexa = require ('./assets_alexa');
 
 //initalize the elastic search client
 
 
-module.exports = function(queryString) {
+module.exports = function(queryString, format) {
 
   return new Promise(function (resolve, reject){
     
@@ -18,13 +21,19 @@ module.exports = function(queryString) {
     var hits;
   //edit
     console.log('querystring in assets.js: '+queryString);
+    console.log('format in assets.js: '+ format);
     client.search({
       q: queryString,
       _sourceExclude: '*relatedAssets'
     }).then(function (body) {
       hits = body.hits.hits;
-      console.log("Hits in Assets.js" + hits);
-      resolve(hits);
+      //console.log("Hits in Assets.js" + hits);
+      if (format !== 'alexa'){
+             resolve(hits); 
+      } else {
+        resolve(formatAlexa(hits));
+      }
+
     }, function (error) {
       console.trace(error.message);
     });
