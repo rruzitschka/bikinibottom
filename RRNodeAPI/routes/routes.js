568229f9cd4165b9f0edb9d50d7dfe42d56c642a
RobertR <robert.ruzitschka@bydeluxe.com>
2018-05-18 22:48:35 +0200

    introduces format parameter in assets route

--- RRNodeAPI/routes/routes.js
+++ RRNodeAPI/routes/routes.js
@@ -22,12 +22,14 @@ var appRouter = function (app) {
     console.log('in routes.js assets service called');
     //req.query.q gives access to the value of the q parameter in the URL
     let queryString=req.query.q;
+    let format = req.query.format;
    
     console.log('in routes.js' + assets(queryString));
     
-    assets(queryString).then(function(data) {
-      console.log('Hits found:'+ data.length);
-      res.status(200).send(data);
+    assets(queryString, format).then(function(data) {
+  
+        res.status(200).send(data);
+ 
     })
     .catch( function() {
       console.log('Promise catched');
@@ -160,6 +162,11 @@ var appRouter = function (app) {
 
 
 });
+
+
+
+
+
 
 
 }
