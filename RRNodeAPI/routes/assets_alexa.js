a7963f20b2a8073582b44e8039e4bafb8d851ce7
RobertR <robert.ruzitschka@bydeluxe.com>
2018-05-24 14:05:59 +0200

    Implemented new API call responses and ssupport for synopsis parameter

--- RRNodeAPI/routes/assets_alexa.js
+++ RRNodeAPI/routes/assets_alexa.js
@@ -3,15 +3,39 @@
 
 var _ = require('underscore');
 
-module.exports = function (hits) {
+module.exports = function (hits, synopsisFlag) {
     var TitleList = [];
     hits.forEach(element => {
         let AssetTitle = {};
-        let assetName = element._source.originalName;
-        console.log(assetName);
-        AssetTitle.name = element._source.originalName;
-        AssetTitle.id = element._id;
-        TitleList.push(AssetTitle);
+        if(synopsisFlag === true){
+            AssetTitle.name = element._source.originalName;
+            AssetTitle.language = element._source.mediaLangs[0].langId;
+            AssetTitle.synopsis = element._source.mediaLangs[0].synopsis.long;
+            AssetTitle.id = element._id;
+ 
+
+
+         } else {
+            
+            let actorsArray = element._source.mediaLangs[0].actors;
+            let actorsNameArray = [];
+            AssetTitle.name = element._source.originalName;
+            AssetTitle.language = element._source.mediaLangs[0].langId;
+            AssetTitle.ProductionYear = element._source.productionYear;
+            actorsArray.forEach((actor) => {
+                console.log (_.pick(actor, 'name'));
+                actorsNameArray.push(_.pick(actor, 'name'));
+            });
+            AssetTitle.actors = actorsNameArray;
+            AssetTitle.id = element._id;
+            console.log(AssetTitle.actors);
+ 
+
+
+      }
+     
+      TitleList.push(AssetTitle);
+
     });
     return(TitleList);
 }
