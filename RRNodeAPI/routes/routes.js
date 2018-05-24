6cb3f4bb69f21e4bcbec150258dec7c2891a44e5
RobertR <robert.ruzitschka@bydeluxe.com>
2018-05-24 14:07:18 +0200

    Implemented new route for Alexa API, changed some console.logs

--- RRNodeAPI/routes/routes.js
+++ RRNodeAPI/routes/routes.js
@@ -4,6 +4,7 @@ var assetSynopsis = require('./assetsynopsis.js');
 var assetActors = require('./assetactors.js');
 var assetCrew = require('./assetcrew.js');
 var findLangIndex = require ('../findlangindex.js');
+var _ = require('underscore');
 
 
 var appRouter = function (app) {
@@ -19,12 +20,12 @@ var appRouter = function (app) {
 
 
   app.get("/v1/assets", function (req, res) {
-    console.log('in routes.js assets service called');
+
     //req.query.q gives access to the value of the q parameter in the URL
     let queryString=req.query.q;
     let format = req.query.format;
    
-    console.log('in routes.js' + assets(queryString));
+    console.log('Assets requested with query string: ' + queryString);
     
     assets(queryString, format).then(function(data) {
   
@@ -40,14 +41,14 @@ var appRouter = function (app) {
  // get an asset with a specific assetID 
   
   app.get("/v1/assets/:id", function (req, res) {
-    console.log('in routes.js assets/id service called');
+ 
     //req.params.id gives access to the value of the id parameter in the URL
     let assetId=req.params.id;
-    console.log('asset id:'+ assetId);
+    console.log('Asset Data requested for Asset with asset id: '+ assetId);
     //this calls the assetID function
     assetid(assetId).then(function(data) {
       if(!data) {
-        res.status(400).send('Asset not found');
+        res.status(404).send('Asset not found');
       } else {
         res.status(200).send(data);
       }
@@ -62,12 +63,13 @@ var appRouter = function (app) {
  // get the synopsis for an asset with a specific assetID
 
   app.get("/v1/assets/:id/synopsis", function (req, res) {
-    console.log('in routes.js assets/id service called');
+
     //req.params.id gives access to the value of the id parameter in the URL
     let assetId=req.params.id;
     // parameters lang and length hold the desired language 
     let synopsisLang = req.query.lang;
     
+    console.log('Synopsis Data requested for Asset with asset id: '+ assetId);
 
     // set defaults in case parameters are not given
 
@@ -75,16 +77,12 @@ var appRouter = function (app) {
       synopsisLang = "en";
     }
 
- 
-
-    console.log('asset id:'+ assetId);
-    console.log('Synopsis Language Param: '+ synopsisLang);
     //this calls the assetID function
     assetSynopsis(assetId, synopsisLang)
     .then(function(data) {
 
       if(!data) {
-        res.status(400).send('Asset not found');
+        res.status(404).send('Asset not found');
       } else {
         res.status(200).send(data);
       }
@@ -99,7 +97,7 @@ var appRouter = function (app) {
 
 
   app.get("/v1/assets/:id/actors", function (req, res) {
-    console.log('in routes.js assets/id service called');
+
     //req.params.id gives access to the value of the id parameter in the URL
     let assetId=req.params.id;
     // parameters lang and length hold the desired language 
@@ -107,19 +105,18 @@ var appRouter = function (app) {
 
     // set defaults in case parameters are not given
 
+    console.log('Actor Data requested for Asset with asset id: '+ assetId);
     if (actorsLang === undefined){
       actorsLang = "en";
     }
 
-    console.log('asset id:'+ assetId);
-    console.log('Actors Language Param: '+ actorsLang);
     //this calls the assetID function
     assetActors(assetId, actorsLang)
     .then(function(data) {
       if(data.length > 0) {
         res.status(200).send(data);
       } else {
-        res.status(400).send('Asset not found');
+        res.status(404).send('Asset not found');
       }
     })
     .catch( function() {
@@ -131,29 +128,24 @@ var appRouter = function (app) {
  // get the crew for an asset with a specific asset ID
 
     app.get("/v1/assets/:id/crew", function (req, res) {
-      console.log('in routes.js assets/id service called');
       //req.params.id gives access to the value of the id parameter in the URL
       let assetId=req.params.id;
       // parameters lang and length hold the desired language 
       let crewLang = req.query.lang;
       
-  
       // set defaults in case parameters are not given
-  
+      console.log('Crew Data requested for Asset with asset id: '+ assetId);
       if (crewLang === undefined){
         crewLang = "en";
       }
 
-  
-      console.log('asset id:'+ assetId);
-      console.log('Crew Language Param: '+ crewLang);
       //this calls the assetID function
       assetCrew(assetId, crewLang)
       .then(function(data) {
         if(data.length > 0) {
         res.status(200).send(data);
       } else {
-        res.status(400).send('Asset not found');
+        res.status(404).send('Asset not found');
       }
       })
       .catch( function() {
@@ -163,7 +155,25 @@ var appRouter = function (app) {
 
 });
 
+// Alexa API
 
+app.get("/v1/alexa/assets", function (req, res) {
+
+  //req.query.q gives access to the value of the q parameter in the URL
+  let queryParams=req.query;
+ 
+  //console.log('Assets requested with query string: ' + queryString);
+  
+  assets(queryParams, 'alexa').then(function(data) {
+
+      res.status(200).send(data);
+
+  })
+  .catch( function() {
+    console.log('Promise catched');
+  })
+
+});
 
 
 
